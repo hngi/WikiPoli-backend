0a75461f640a80deaad24eaa0e6eccb531d40725
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-22 21:33:45 +0100

    added the get method

---
+++ Api/PostAdmin.php
@@ -0,0 +1,29 @@
+<?php
+    require_once('../autoloader.php');
+    
+    use Helper\Admin as Admin;
+
+    //echo(Admin::say_hello());
+
+    /**
+     * 
+     */
+    class PostAdmin {
+    	$db;
+    	
+    	function __construct()
+    	{
+    		$this->db = new Database();
+    	}
+
+        public function get($id = NULL)
+    	{
+    		
+
+
+
+    	}
+        
+    }
+   
+?>

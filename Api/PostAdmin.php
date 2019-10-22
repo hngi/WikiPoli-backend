49216934447504164878c17daad160e7ea456734
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-22 21:46:50 +0100

    got post from db and returned back data in json

--- Api/PostAdmin.php
+++ Api/PostAdmin.php
@@ -13,22 +13,37 @@
     	
     	function __construct()
     	{
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Headers: *");
+        header("Access-Control-Allow-Method: *");
+        header('Content-Type: application/json');
+
     		$this->db = new Database();
     	}
 
         public function get($id = NULL)
     	{
     		
+          $post = $this->db->select("SELECT * FROM posts WHERE post_id =$id;");
+          if (!$post) {
 
+          echo json_encode(['error'=>1,'message'=>'No Post Yet'],true);
 
+          }else{
 
+          echo json_encode(['error'=>0,'message'=>'Post Gotten Successfully','post'=>$post[0]],true);
+
+          }
     	}
+
         public function publish()
         {
-            # code...
+            
+
+            
         }
 
-        
+
         public function unpublish($id)
         {
                     # code...

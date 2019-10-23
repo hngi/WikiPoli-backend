2aca607c1f2e0ff9f840cc9df81b34a8fb0e1075
bringforthjoy <adelugba.emma@gmail.com>
2019-10-23 09:23:54 +0100

    jwt authentication fixed

--- Api/addAdmin.php
+++ Api/addAdmin.php
@@ -2,77 +2,81 @@
     require_once('../autoloader.php');
     
     use Helper\Database as DB;
+    use Helper\Jwt_client as jwt;
+
     if($_SERVER['REQUEST_METHOD']=='POST'){
 
-        if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password']) && !empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['password'])){
-
-            //$password=md5(htmlspecialchars($_POST['password']));
-            //$name= htmlspecialchars($_POST['name']);
-            //$email=htmlspecialchars($_POST['email']);
-
-            $conn=DB::db_connect();
-
-            if($result==FALSE){
-
-                $res=DB::addAdmin();
-
-                if($res){
-
-                    $data=[
-                        'res'=>'Admin Created Successfully',
-                        'status'=>200
-                    ];
+            if(isset($_POST['token']) && !empty($_POST['token'])){
             
-                    //http_response_code(404); 
-                    return json_encode($data);
+                $arr=jwt::decode($_POST['token']);
+
+                if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password']) && !empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['password'])){
+
+                //$password=md5(htmlspecialchars($_POST['password']));
+                //$name= htmlspecialchars($_POST['name']);
+                //$email=htmlspecialchars($_POST['email']);
+
+                    $conn=DB::db_connect();
+
+                    if($result==FALSE){
+
+                        $res=DB::addAdmin();
+
+                        if($res){
+
+                            $data=[
+                                'res'=>'Admin Created Successfully',
+                                'status'=>200
+                            ];
+                    
+                            //http_response_code(404); 
+                            return json_encode($data);
+
+                        }else{
+
+                            $data=[
+                                'res'=>'Error Creating Admin',
+                                'status'=>404
+                            ];
+                    
+                            //http_response_code(404); 
+                            return json_encode($data);
+                        }
+            
+
+                    }else{
+
+                        $data=[
+                            'res'=>'Admin already Exists',
+                            'status'=>404
+                        ];
+                
+                        //http_response_code(404); 
+                        return json_encode($data);
+
+                    }
+                  
 
                 }else{
 
                     $data=[
-                        'res'=>'Error Creating Admin',
+                        'res'=>'Invalid Credentials',
                         'status'=>404
                     ];
             
                     //http_response_code(404); 
                     return json_encode($data);
+
                 }
-    
-
             }else{
-
                 $data=[
-                    'res'=>'Admin already Exists',
+                    'res'=>'token not set',
                     'status'=>404
                 ];
-        
-                //http_response_code(404); 
+                
                 return json_encode($data);
-
             }
-
-
-        }else{
-
-            $data=[
-                'res'=>'Invalid Credentials',
-                'status'=>404
-            ];
     
-            //http_response_code(404); 
-            return json_encode($data);
-
-        }
-
-    }else{
-
-        $data=[
-            'res'=>'Page not found',
-            'status'=>404
-        ];
-
-        //http_response_code(404); 
-        return json_encode($data);
-        
-    }
+}
 
 ?>
--- Api/makeAdmin.php
+++ Api/makeAdmin.php
@@ -2,9 +2,15 @@
     require_once('../autoloader.php');
     
     use Helper\Database as DB;
+    use Helper\Jwt_client as jwt;
+
     if($_SERVER['REQUEST_METHOD']=='POST'){
 
-        $conn=DB::db_connect();
+        if(isset($_POST['token']) && !empty($_POST['token'])){
+		
+            $arr=jwt::decode($_POST['token']);
+
+            $conn=DB::db_connect(); 
 
         
 
@@ -30,7 +36,14 @@
                     //http_response_code(404); 
                     return json_encode($data);
                 }
-        
+        }else{
+            $data=[
+                'res'=>'token not set',
+                'status'=>404
+            ];
+            
+            return json_encode($data);
+        }
 
     }
 

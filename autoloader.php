acd699748fd857d4e3db2706de5f76cbb7d61501
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-23 09:45:42 +0100

    changed env

--- autoloader.php
+++ autoloader.php
@@ -1,10 +1,10 @@
 <?php
-    
+
     require('vendor/autoload.php');
     $ext=dirname(__FILE__);
     $ext=$ext."\Helpers\*.php";
     $ext=str_replace('\\', '/', $ext);
-    
+
     foreach (glob($ext) as $filename)
     {
         include_once($filename);

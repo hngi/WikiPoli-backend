bc0541a3963c3c3ea7ef977e4cdd6bba03825db8
Adekanye Jubril <37573577+tyroklone@users.noreply.github.com>
2019-10-23 05:27:36 -0700

    Edited the forgot password class for usage in the api

--- Helpers/ForgotPassword.php
+++ Helpers/ForgotPassword.php
@@ -11,9 +11,6 @@ final class ForgotPassword
   * Begin the class properties
   */
  protected static $valid = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
- protected static $default = array("email", "submit");
- public static $errors = array();
- public static $response;
 
  /*
   * Begin The Methods
@@ -70,67 +67,51 @@ final class ForgotPassword
   }
  }
 
-
- public static function forgot_password($input = array(), $req = true)
+/*
+ * Create the update password method
+ */
+public static function update_password($email)
+{
+ $echeck = self::email_check($email);
+ if(!$echeck)
  {
-  if(!isset($input) || !is_array($input))
-   {
-    $input = self::$defult;
-   }
+  // Connect to the database
+  $db = DB::db_connect();
+  
+  // New Generated password
+  $np = self::random_char(6);
+  // Sanitize and decode
+  $np = htmlspecialchars(md5($np));
+  $update_sql = "UPDATE users SET password = '$np' WHERE email = '$email'";
+  mysqli_query($db, $update_sql);
+  
+  return true;
+ }
+}
 
-  // Lets begin the fun stuff
-  if(is_array($input))
-   {
-    // Lets process the array
-    // You may wonder why we need an array
-    // This is because i have not seen the forgot password markup
-    // And i don't know the name of input fields
-   
-    if($req == true)
-     {
-      // Use the POST request method
-      $email = $_GET[$input[0]];
-      $submit = $_GET[$input[1]];
-     }
-      else
-     {
-      // Use the POST request method
-      $email = $_POST[$input[0]];
-      $submit = $_POST[$input[1]];
-     }
+/*
+ * Create the method for sending mail to user
+ */
+public static function send_email($email)
+{
+ // Connect to the database
+ $db = DB::db_connect();
+  
+ $sql = "SELECT * FROM users WHERE email = '$email'";
+ $query = mysqli_query($db, $sql);
+ while($info = mysql_fetch_array($query))
+ {
+  $name = $info['name'];
+  $email = $info['email'];
+ }
 
-   if(isset($submit))
-    {
-     $echeck = self::email_check($email);
-     if(!$echeck)
-      {
-       self::$errors[] = "Email does not exist in our database.";
-       // Terminate the script
-       exit();
-      }
-      
-     if($echeck)
-      {
-       // New Generated password
-       $np = self::random_char(6);
-       $update_sql = "UPDATE users SET password = '$np' WHERE email = '$email'";
-      }
-
-    $sql = "SELECT * FROM users WHERE email = '$email'";
-    $query = mysqli_query($db, $sql);
-    while($info = mysql_fetch_array($query))
-     {
-      $name = $info['name'];
-      $email = $info['email'];
-     }
-
-    $new_pass = $np;
-    $to = $email; 
-    $from = 'WIKIPOLI'; 
-    $fromName = 'Wikipoli Team'; 
-    $subject = "Your Wikipoli Account New Password"; 
+ $new_pass = $np;
+ $to = $email; 
+ $from = 'WIKIPOLI'; 
+ $fromName = 'Wikipoli Team'; 
+ $subject = "Your Wikipoli Account New Password"; 
  
-    $htmlContent = ' 
+ $htmlContent = ' 
     <html> 
     <head> 
     <title>New Wikipoli Password</title> 
@@ -157,26 +138,24 @@ final class ForgotPassword
     </body> 
     </html>'; 
  
-   // Set content-type header for sending HTML email 
-   $headers = "MIME-Version: 1.0" . "\r\n"; 
-   $headers .= "Content-type: text/html;charset=UTF-8" . "\r\n"; 
+ // Set content-type header for sending HTML email 
+ $headers = "MIME-Version: 1.0" . "\r\n"; 
+ $headers .= "Content-type: text/html;charset=UTF-8" . "\r\n"; 
  
-   // Additional headers 
-   $headers .= 'From: '.$fromName.'<'.$from.'>' . "\r\n"; 
-   $headers .= 'Cc: '".$email."' . "\r\n"; 
-   $headers .= 'Bcc: '".$email."' . "\r\n"; 
+ // Additional headers 
+ $headers .= 'From: '.$fromName.'<'.$from.'>' . "\r\n"; 
+ $headers .= 'Cc: '".$email."' . "\r\n"; 
+ $headers .= 'Bcc: '".$email."' . "\r\n"; 
  
-   if(mail($to, $subject, $htmlContent, $headers))
-    { 
-     $response = "Your New Password Has Been Sent To Your Email.";
-    }
-     else
-    {
-     self::$errors[] = "Error Sending Email.";
-    }
-   }
-  }
+ if(mail($to, $subject, $htmlContent, $headers))
+ { 
+  return true;
  }
+  else
+ {
+  return false;
+ }
+}
 }
 
 ?>

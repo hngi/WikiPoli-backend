0400273a3329de2c38c152c1cfa092088c900130
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-23 10:20:30 +0100

    added jwt validation to publish method

--- Api/PostAdmin.php
+++ Api/PostAdmin.php
@@ -43,6 +43,11 @@
         public function publish($id=NULL)
         {
 
+	if(isset($_POST['token']) && !empty($_POST['token'])){
+
+		$arr=jwt::decode($_POST['token']);
+
+
     $post = $this->db->select("SELECT * FROM posts WHERE post_id =$id;");
           if (!$post) {
           echo json_encode(['error'=>1,'message'=>'No Such Post','post_id'=>$id],true);
@@ -61,8 +66,11 @@
         }
                   $this->db->close();
 
-        }
+        }else{
 
+ echo json_encode(['error'=>1,'message'=>'Invalid token','post_id'=>$id],true);
+    }
+        }
 
         public function unpublish($id=NULL)
         {

ef358ed3b705e958a9740a162bea492c6f64bb76
Sydney Ekpo <sydney.j.ekpo@gmail.com>
2019-10-23 20:06:20 +0100

    backup api

---
+++ wikipoli-api/Api/index.php
@@ -0,0 +1,10 @@
+<?php
+    require_once('../autoloader.php');
+    
+    use Helper\Jwt_client as jwt;
+    use Helper\Database as DB;
+
+    DB::db_connect();
+   
+   
+?>
---
+++ wikipoli-api/Helpers/superAdmin.php
@@ -0,0 +1,18 @@
+<?php
+
+
+/* 
+
+    This is the superAdmin Class 
+*/
+
+    namespace Helper;
+
+    class superAdmin{
+
+        public static function say_hello(){
+
+            return "hello";
+        }
+    }
+?>

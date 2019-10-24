81561e515a6bdeee9c6d680b3e4a723dfe983d09
Dollypee <olaiyadolapo@gmail.com>
2019-10-24 18:23:42 +0100

    corrected block and unblock users

--- Api/unblockuser.php
+++ Api/unblockuser.php
@@ -6,60 +6,61 @@ use Helper\Jwt_client as jwt;
 
 if($_SERVER['REQUEST_METHOD'] == 'POST')
 {
- // DB connection
- $db = DB::db_connect();
- 
- if(isset($_POST['token']) && isset($_POST['token']))
+ if(isset($_POST['token']) && !empty($_POST['token']))
  {
   $array = jwt::decode($_POST['token']);
+  // DB connection
+  $db = DB::db_connect();
   
   if(DB::confirm_id($db, $array['data']->id))
   {
-     if(DB::confirm_admin($db, $array['data']->id))
-   {
-    if(isset($_POST["uid"]))
-    {
+     if(DB::confirm_admin($db, $array['data']->id)){
+    if(isset($_POST["uid"]) && !empty($_POST["uid"])){
      $uid = $_POST["uid"];
      $unblock = DB::unblock_user($db, $uid);
-     if($unblock == true)
-     {
+     if($unblock == true){
        $data = [
          'res' => 'User Unblocked Successfully.',
-         'status' => 200,
-         'data' => $uid
+         'status' => 200
        ];
+
+       echo json_encode($data);
      }
-      else
-     {
+      else{
       $data = [
          'res' => 'An Error Occured.',
          'status' => 404
        ];
+
+       echo json_encode($data);
      }
-     }
-      else
-     {
+    }
+      else{
           $data = [
               'res' => 'Unauthorized Access',
                 'status' => 403
-          ];        
+          ];
+          
+          echo json_encode($data);
         }
     }
-    else
-   {
+    else{
         $data = [
          'res'=>'Invalid User',
          'status'=>404
      ];
+
+     echo json_encode($data);
     }
   }
-   else
-  {
+   else{
      $data = [
         'res' => 'No token was sent',
         'status' => 404
     ];
- }
+
+    echo json_encode($data);
+  }
  }
   else
  {
@@ -67,8 +68,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
      'res' => 'Invalid Request Method',
     'status'=>404
     ];
-} 
 
-echo json_encode($data);
+    echo json_encode($data);
+  } 
 }
 ?>

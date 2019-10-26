0dc3dfc1d57da50d05da4c3a287b476b567e1438
Ubuntu <ubuntu@ip-172-31-93-242.ec2.internal>
2019-10-26 01:40:11 +0000

    install update

--- autoloader.php
+++ autoloader.php
@@ -10,8 +10,9 @@
         include_once($filename);
     }
 
-    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Origin:req.header('Origin') || *");
     header("Access-Control-Allow-Headers: X-Requested-With");
     header("Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS");
-    header('Content-Type: application/json');
+    header("Content-Type: application/json");
+    header("Access-Control-Allow-Credentials: true");
 ?>

ac5c7d9b60e459395996b960ab61288b18202215
eshy008 <eshy008@gmail.com>
2019-10-26 12:29:49 +0100

    Created the edit_post.php for users to edit their post's

---
+++ Api/edit_post.php
@@ -0,0 +1,84 @@
+<?php
+require_once('../autoloader.php');
+use Helper\Database as DB;
+use Helper\Jwt_client as jwt;
+
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	if(isset($_POST['token']) && !empty($_POST['token'])){
+		
+		$arr=jwt::decode($_POST['token']);
+		
+		$conn=DB::db_connect();
+		if(DB::confirm_id($conn,$arr['data']->id)){
+			if(isset($_POST["post"])  && !empty($_POST["post"]) && isset($_POST["topic"])  && !empty($_POST["topic"]) ){
+				
+				$post=$_POST["post"];
+				$topic=$_POST["topic"];
+				$id=$arr['data']->id;
+        if(!(DB::user_blocked($conn,$id))){
+          if(DB::edit_post($conn,$id,$post,$topic)==TRUE){
+            $data=[
+              'res'=>'Post Updated Successfully',
+              'status'=>200
+            ];
+              
+            
+            echo json_encode($data);
+          }else{
+					$data=[
+						'res'=>'Update failed',
+            'status'=>404,
+            'err'=>mysqli_error($conn)
+					];
+						
+					
+					echo json_encode($data);
+				}
+			}else{
+        $data=[
+          'res'=>'Your account is currently blocked',
+          'status'=>200,
+          'err'=>mysqli_error($conn)
+        ];
+          
+        
+        echo json_encode($data);
+      }
+    }else{
+			$data=[
+				'res'=>'Invalid Credentials',
+				'status'=>404
+			];
+				
+			
+			echo json_encode($data);
+		}
+		}else{
+			$data=[
+				'res'=>'Invalid User',
+				'status'=>404
+			];
+				
+			//http_response_code(404); 
+			echo json_encode($data);
+		}
+	}else{
+		$data=[
+			'res'=>'No token was sent',
+			'status'=>404
+		];
+	
+		//http_response_code(404); 
+		echo json_encode($data);
+	}
+}else{
+	$data=[
+		'res'=>'Invalid Request Method',
+		'status'=>404
+	];
+	//http_response_code(404); 
+	echo json_encode($data);
+}
+
+?>
